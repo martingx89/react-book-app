802f8e47884352186be1fc06184d725736aa82a6
Marcin Adamiak <marcin_89@outlook.com>
2023-05-31 16:38:04 +0200

    Init redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <Provider store={store}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </Provider>
 );
---
+++ src/redux/store.js
@@ -0,0 +1,15 @@
+import { createStore } from 'redux';
+
+const reducer = (state, action) => {
+  return state;
+};
+
+const initialState = {};
+
+const store = createStore(
+  reducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
