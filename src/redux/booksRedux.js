45b6e3580c2f7dea105dea745eb5edcb8992ab96
Marcin Adamiak <marcin_89@outlook.com>
2023-06-08 21:36:36 +0200

    Add new action

--- src/redux/booksRedux.js
+++ src/redux/booksRedux.js
@@ -7,11 +7,13 @@ export const getAllBooks = (state) => state.books;
 const createActionName = (name) => `app/books/${name}`;
 const REMOVE_BOOK = createActionName('REMOVE_BOOK');
 const ADD_BOOK = createActionName('ADD_BOOK');
+const UPDATE_BOOKS = createActionName('UPDATE_BOOKS');
 
 // action creators
 
 export const removeSelectedBook = (payload) => ({ type: REMOVE_BOOK, payload });
 export const addNewBook = (payload) => ({ type: ADD_BOOK, payload });
+export const updateBooks = (payload) => ({ type: UPDATE_BOOKS, payload });
 
 const booksRedux = (statePart = [], action) => {
   switch (action.type) {
@@ -19,6 +21,8 @@ const booksRedux = (statePart = [], action) => {
       return statePart.filter((book) => book.id !== action.payload);
     case ADD_BOOK:
       return [...statePart, { ...action.payload, id: shortid() }];
+    case UPDATE_BOOKS:
+      return [...action.payload];
     default:
       return statePart;
   }
