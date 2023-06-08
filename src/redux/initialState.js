7ca6de16f25a7290c7ba6263daf782ce230b1eb4
Marcin Adamiak <marcin_89@outlook.com>
2023-06-08 21:31:29 +0200

    Clean local state

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,8 +1,5 @@
 const initialState = {
-  books: [
-    { id: 1, title: 'of Mice and Men', author: 'John Steinbeck' },
-    { id: 2, title: 'the Witcher', author: 'Andrzej Sapkowski' },
-  ],
+  books: [],
 };
 
 export default initialState;
