1073533d31693a3fcb27229d27eae817e4fffeb7
Marcin Adamiak <marcin_89@outlook.com>
2023-06-11 13:02:20 +0200

    Add compose, thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import booksRedux from './booksRedux';
 import initialState from './initialState';
 
@@ -9,7 +10,10 @@ const reducer = combineReducers({
 const store = createStore(
   reducer,
   initialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+  )
 );
 
 export default store;
